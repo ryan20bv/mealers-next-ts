478e1149a59d84f96ef155619e0fa58b8f386f81
ryan20bv <ryan20bv@gmail.com>
2024-05-12 08:09:51 +0800

    created meal view

--- src/components/views/meals/modules/meal-item.tsx
+++ src/components/views/meals/modules/meal-item.tsx
@@ -19,26 +19,34 @@ export default function MealItem({
 	creator
 }: PropsType) {
 	return (
-		<article className={classes.meal}>
+		<article
+			className={`${classes.meal} flex flex-col justify-center h-full rounded-md overflow-hidden transition-all ease-in-out text-[#ddd6cb]`}
+		>
 			<header>
-				<div className={classes.image}>
+				<div className={'relative h-[15rem]'}>
 					<Image
 						src={image}
 						alt={title}
 						fill
 						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 						priority
+						className='object-cover'
 					/>
 				</div>
-				<div className={classes.headerText}>
-					<h2>{title}</h2>
-					<p>by {creator}</p>
+				<div className={`${classes.headerText} pt-2 pb-0 px-4`}>
+					<h2 className='m-0 text-2xl font-bold'>{title}</h2>
+					<p className='m-0 text-xs text-[#cfa69b]  italic'>by {creator}</p>
 				</div>
 			</header>
-			<div className={classes.content}>
-				<p className={classes.summary}>{summary}</p>
-				<div className={classes.actions}>
-					<Link href={`/meals/${slug}`}>View Details</Link>
+			<div className='flex flex-col justify-between h-full'>
+				<p className='pt-4 pb-0 px-4'>{summary}</p>
+				<div className={`${classes.actions} p-4 text-right`}>
+					<Link
+						href={`/meals/${slug}`}
+						className='inline-block mt-4 px-4 py-2 rounded-[0.5rem] text-white font-bold no-underline'
+					>
+						View Details
+					</Link>
 				</div>
 			</div>
 		</article>
